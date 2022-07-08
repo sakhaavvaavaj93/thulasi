5719eec1ea78552e09d3aa9f994fd6a64e08f209
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-07-08 13:31:47 +0530

    Add files via upload

---
+++ thulasi/__main__.py
@@ -0,0 +1,25 @@
+
+
+
+import glob
+from pathlib import Path
+from thulasi.utils import load_plugins
+import logging
+from thulasi import thulasi
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.INFO)
+
+path = "thulasi/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins(plugin_name.replace(".py", ""))
+    
+print("Successfully Started Bot!")
+print("Visit @kovilakam")
+
+if __name__ == "__main__":
+    thulasi.run_until_disconnected()
