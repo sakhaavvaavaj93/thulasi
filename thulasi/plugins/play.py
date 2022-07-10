591a9c72e970156d35d7f92d8c524f90ef8fdbe9
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-07-11 02:35:36 +0530

    Update play.py

--- thulasi/plugins/play.py
+++ thulasi/plugins/play.py
@@ -420,9 +420,9 @@ async def vc_playlist(event, perm):
                 hmm = chat_queue[x][0]
                 hmmm = chat_queue[x][2]
                 hmmmm = chat_queue[x][3]
-         if PLAYLIST = PLAYLIST + "\n" + \
+          if PLAYLIST = PLAYLIST + "\n" + \
                     f"**#{x}** - [{hmm}]({hmmm}) | `{hmmmm}`"
-            await xnxx.edit(PLAYLIST, link_preview=False)
+             await xnxx.edit(PLAYLIST, link_preview=False)
     else:
         await event.reply("**Ntg is Streaming**")
 
