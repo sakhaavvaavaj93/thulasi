2083867df9bc77ce0ccbf503c58f9e369ae30e03
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-07-11 13:14:58 +0530

    Update play.py

--- thulasi/plugins/play.py
+++ thulasi/plugins/play.py
@@ -409,12 +409,13 @@ async def vc_playlist(event, perm):
     if chat_id in QUEUE:
         chat_queue = get_queue(chat_id)
         if len(chat_queue) == 1:
-        xnxx = await event.reply(
+        await event.reply(
                 f"**�PlAYLIST:**\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`",
                 link_preview=False,
             )
+    else:
         PLAYLIST = f"**🎧 PLAYLIST:**\n**• [{chat_queue[0][0]}]({chat_queue[0][2]})** | `{chat_queue[0][3]}` \n\n**• Upcoming Streaming:**"
-      elif  l = len(chat_queue)
+        if  l = len(chat_queue)
             for x in range(1, l):
                 hmm = chat_queue[x][0]
                 hmmm = chat_queue[x][2]
