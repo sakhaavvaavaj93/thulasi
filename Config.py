59cfa3c40e2c869a08e65b050328a7b7c3fe0aed
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-07-14 00:47:12 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "16556281"))
-    API_HASH = os.environ.get("API_HASH", "ee38ed3a893001a782f675c6f747ba09")
+    API_ID = int(os.environ.get("APP_ID", "14071925"))
+    API_HASH = os.environ.get("API_HASH", "5f14a90b6f505e80e8e25f061ad51e42")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5567453202:AAGUBvDK8pKmB3zxks3ZZOIdx8rbG229ufw")
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", "ENABLE")
@@ -12,4 +12,4 @@ class Config(object):
     CHANNEL = os.environ.get("CHANNEL", "KANTHARI_WRITINGS")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/1ec66485a71a68fe15392.png")
     CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/1aaf04b524b231ae0d7b1.png")
-    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "5415085732"))
+    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "5562423241"))
